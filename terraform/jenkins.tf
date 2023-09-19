70d0be1cafb547ef675696e5a1182a976b8d9ffc
dfranceschelli <franceschelli@gmail.com>
2023-09-19 09:26:18 +0200

    Update jenkins.tf

--- terraform/jenkins.tf
+++ terraform/jenkins.tf
@@ -25,17 +25,32 @@ provider "aws" {
   secret_key = "BndRQNgoOtN12G/reUkZcPIuvDgNdNIQ5qF1rbG+"
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+resource "aws_security_group" "jenkins-sg" {
+  name        = "jenkins-sg"
+  description = "Allow inbound SSH and HTTP"
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-  owners = ["099720109477"] # Canonical
+
+  ingress {
+     description = "HTTP"
+     from_port   = 8080
+     to_port     = 8080
+     protocol    = "tcp"
+     cidr_blocks = ["0.0.0.0/0"]
+   }
+   
+  egress {
+     from_port   = 0
+     to_port     = 0
+     protocol    = "-1"
+     cidr_blocks = ["0.0.0.0/0"]
+   }
 }
 
 resource "aws_key_pair" "ansible-key" {
@@ -44,12 +59,17 @@ resource "aws_key_pair" "ansible-key" {
 }
 
 resource "aws_instance" "jenkins" {
-  ami           = data.aws_ami.ubuntu.id
+  ami           = "ami-053b0d53c279acc90"
   instance_type = "t2.micro"
   key_name = "ansible-key"
   tags = {
     Name = "jenkins"
   }
+}
+
+resource "aws_network_interface_sg_attachment" "sg_attachment1" {
+  security_group_id = aws_security_group.jenkins-sg.id
+  network_interface_id = aws_instance.jenkins.primary_network_interface_id
 }
 
 output "private_ip" {
