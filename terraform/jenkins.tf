2f2cc8285731ed104f4dd4ea7dce74e68ccc9d12
dfranceschelli <davide.franceschelli@vodafone.com>
2023-09-19 07:06:04 +0000

    terraform aws configuration

---
+++ terraform/jenkins.tf
@@ -0,0 +1,25 @@
+terraform {
+
+  required_providers {
+
+    aws = {
+
+      source = "hashicorp/aws"
+
+      version = "5.17.0"
+
+    }
+
+  }
+
+}
+
+
+
+provider "aws" {
+
+  # Configuration options
+  
+  access_key = "AKIARONOYQPLB2JDO66D"
+  secret_key = "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"
+}
